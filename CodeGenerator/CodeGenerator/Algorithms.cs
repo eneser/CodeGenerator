160249f41af0564fd0e0e46d31352057dcfe1bf5
Enes <enesercan93@gmail.com>
2023-02-17 10:06:12 +0300

    Code Generator First Commit
    Belirli koşullarla kod üretip, üretilen kodları kontrol eden uygulamadır.

---
+++ CodeGenerator/CodeGenerator/Algorithms.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    internal class Algorithms
+    {
+
+        public static string GeneratorCode()
+        {
+            string includeCharachters = "ACDEFGHKLMNPRTXYZ234579";
+            Random random = new Random();
+            StringBuilder resultStringBuilder = new StringBuilder();
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (i < 5)
+                {
+                    long TimeTicks = DateTime.Now.Ticks;
+                    int item = int.Parse(TimeTicks.ToString().Substring(17));
+                    resultStringBuilder.Append(includeCharachters[random.Next(item)]);
+                }
+                else
+                    resultStringBuilder.Append(includeCharachters[random.Next(includeCharachters.Length)]);
+            }
+            return resultStringBuilder.ToString();
+        }
+
+        public static bool CheckCode(string code)
+        {
+            Regex rg = new Regex("^([ACDEFGHKLMNPRTXYZ234579]{8})");
+            return rg.IsMatch(code);
+        }
+
+    }
+}
---
+++ CodeGenerator/CodeGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using ConsoleApp1;
+
+
+for (int i = 0; i < 1000; i++)
+{
+    var generatedCode = Algorithms.GeneratorCode();
+    var isValid = Algorithms.CheckCode(generatedCode);
+    Console.WriteLine($"Code: {generatedCode} - isValid: {isValid}");
+}
